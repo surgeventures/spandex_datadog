aade9e779109483bdaeab9a112f5765359d9cea5
Artur Sulej <artur@shedul.com>
2024-02-06 16:58:53 +0100

    Bump version to 1.6.2

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule SpandexDatadog.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/surgeventures/spandex_datadog"
-  @version "1.6.1"
+  @version "1.6.2"
 
   def project do
     [
